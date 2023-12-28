1258b9aca9ddfdbb82bca5dc801a4fe102aaaab7
Vikas Singh <80041195+Vikas-Singh-10@users.noreply.github.com>
2023-12-29 02:55:42 +0530

    installed dependencies

--- helpers/passport-config.js
+++ helpers/passport-config.js
@@ -1,5 +1,5 @@
 import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import dotenv from 'dotenv';
 
 dotenv.config();
